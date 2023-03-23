50ff9984ffdaa25fa9d30e1406a7cd10a735f86b
seannn9 <naeskirlu9@gmail.com>
2023-03-23 22:19:51 +0800

    Approved method overloading

--- DSA_quiz1/one_methodOverload.cpp
+++ DSA_quiz1/one_methodOverload.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int prod(int a, int b) {
+    return a*b;
+}
+
+int prod(int a, int b, int c) {
+    return a*b*c;
+}
+
 int main() {
+    cout << prod(10, 20) << endl;
+    cout << prod(10, 20, 30);
     return 0;
 }
