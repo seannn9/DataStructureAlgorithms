7e0c2dba3f12a156915f9aa63fc7778506876572
seannn9 <naeskirlu9@gmail.com>
2023-03-23 22:20:54 +0800

    All cases are covered (zero, negative, positive) outputs are correct

--- DSA_quiz1/two_powerMethod.cpp
+++ DSA_quiz1/two_powerMethod.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+    int num, exp;
+    float prod;
+    cout << "Enter number: ";
+    cin >> num;
+    cout << "Enter exponent: ";
+    cin >> exp;
+    prod = num;
+    if (exp == 0) {
+        cout << num << "^" << exp << " = " << 1 << endl;
+    } else {
+        if (exp < 0) {
+            for (int i = 1; i < (exp*=-1); i++) {
+                prod *= num;
+            }
+            prod = 1/prod;
+        } else {
+            for (int i = 1; i < exp; i++) {
+                prod *= num;
+            }
+        }
+        cout << num << "^" << exp << " = " << prod << endl;
+    }
     return 0;
 }
