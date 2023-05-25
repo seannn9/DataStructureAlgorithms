531d82397b67a3226fa32b4258643a2b6bf0627b
seannn9 <naeskirlu9@gmail.com>
2023-05-25 12:57:00 +0800

    Completed the code for problem 2

--- DSA_act7/Act7_#2/main.cpp
+++ DSA_act7/Act7_#2/main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
+    int arr[10] = {10, 15, 31, 15, 15, 5, 2, 10, 10, 27}, count = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] % 2 == 0) {
+            cout << "Index of " << arr[i] << ": " << i << endl;
+            count++;
+        }
+    }
+
+    cout << "Count of even numbers: " << count << endl;
 
     return 0;
 }
