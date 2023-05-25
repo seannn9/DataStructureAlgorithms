6b301ff32ca157d203ddccd173f998ff8b8f82be
seannn9 <naeskirlu9@gmail.com>
2023-05-25 20:20:52 +0800

    Completed code for problem 4

--- DSA_act7/Act7_#4/main.cpp
+++ DSA_act7/Act7_#4/main.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
+    int num[3][3], val, big = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Enter value: ";
+            cin >> val;
+            num[i][j] = val;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (num[i][j] > big) {
+                big = num[i][j];
+            }
+        } cout << "Biggest in row " << i+1 << ": " << big << endl;
+        big = 0;
+    }
 
     return 0;
 }
