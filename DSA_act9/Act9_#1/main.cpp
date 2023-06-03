1470408f954d25c511ca28a1afb61b55b2adfd4d
seannn9 <naeskirlu9@gmail.com>
2023-06-03 23:54:55 +0800

    Completed code for problem 1

--- DSA_act9/Act9_#1/main.cpp
+++ DSA_act9/Act9_#1/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int main() {
+    stack<int> nums;
+    stack<int> numsA;
+    int num, temp;
 
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter a number: ";
+        cin >> num;
+        nums.push(num);
+    }
+
+    while (!nums.empty()) {
+        temp = nums.top();
+        nums.pop();
+        while (!numsA.empty() && numsA.top() > temp) {
+            nums.push(numsA.top());
+            numsA.pop();
+        }
+        numsA.push(temp);
+        
+    }
+    cout << "\nStack in Ascending order: " << endl;
+    while (!numsA.empty()) {
+        cout << numsA.top() << endl;
+        numsA.pop();
+    }
     return 0;
 }
