1133162fb0c611053456a16c0d2a035da11c933c
seannn9 <naeskirlu9@gmail.com>
2023-06-05 00:14:51 +0800

    completed code for problem 2

--- DSA_act10/Act10_#2/main.cpp
+++ DSA_act10/Act10_#2/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
+    multimap<char, string> names;
+    string name;
+    char gender;
+    for (int i = 0; i < 6; i++) {
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "Enter your gender (M/F): ";
+        cin >> gender;
+        
+        if (gender == 'M' || gender == 'm') {
+            names.insert(pair<char, string>('M', name));
+        } else if (gender == 'F' || gender == 'f') {
+            names.insert(pair<char, string>('F', name));
+        } else {
+            cout << "\nPlease try again";
+            i--;
+        }
+    }
+
+    multimap<char, string>::iterator it = names.begin();
+    cout << "\nWhat gender would you like to display (M/F): ";
+    cin >> gender;
+    
+    if (gender == 'M' || gender == 'm') {
+        cout << "Male:" << endl;
+        for (it; it != names.end(); it++) {
+                if (it->first == 'M' || it->first == 'm') {
+                    cout << it->second << endl;
+                }
+        }
+    } else if (gender == 'F' || gender == 'f') {
+        cout << "Female:" << endl;
+        for (it; it != names.end(); it++) {
+                if (it->first == 'F' || it->first == 'f') {
+                    cout << it->second << endl;
+                }
+        }
+    }
 
     return 0;
 }
