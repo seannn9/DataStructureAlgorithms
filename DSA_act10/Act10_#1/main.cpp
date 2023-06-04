c27c8e92af6361fa481be4396e33e72082445420
seannn9 <naeskirlu9@gmail.com>
2023-06-05 01:13:56 +0800

    added descending order sort

--- DSA_act10/Act10_#1/main.cpp
+++ DSA_act10/Act10_#1/main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
+    int voltage[10], val, temp;
+    for (int i = 0; i < sizeof(voltage)/4; i++) {
+        cout << "Enter voltage value: ";
+        cin >> val;
+        voltage[i] = val;
+    }
 
+    for (int i = 0; i < sizeof(voltage)/4; i++) {
+        for (int j = 0; j < (sizeof(voltage)/4)-i-1; j++) {
+            if (voltage[j] < voltage[j+1]) {
+                temp = voltage[j];
+                voltage[j] = voltage[j+1];
+                voltage[j+1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < sizeof(voltage)/4; i++) {
+        cout << voltage[i] << endl;
+    }
     return 0;
 }
